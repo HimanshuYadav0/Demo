ab68d9abbd69a1a11fd70d1317c8932922dc150f
Himanshu Yadav <103452334+HimanshuYadav0@users.noreply.github.com>
2025-02-24 12:52:36 +0530

    Add files via upload

---
+++ CalculatorService/CalculatorService/Service1.svc.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Policy;
+
+public class CalculatorService : ICalculatorService
+{
+    public double Add(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    public double Subtract(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+}
+
